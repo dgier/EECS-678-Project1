e917c91b1fd3f159756b4461b1dfce6bc828a8af
sboeck <Sean@Macintosh-4.local>
2014-10-05 13:54:17 -0500

    runs executables, testing exit/quit

--- main.c
+++ main.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 
 /*
- * 1. Run executables without arguments (10)
- * 2. Run executables with arguments (10)
- * 3. set for HOME and PATH work properly (5)
- * 4. exit and quit work properly (5)
- * 5. cd (with and without arguments) works properly (5)
- * 6. PATH works properly. Give error messages when the executable is not found (10)
- * 7. Child processes inherit the environment (5)
- * 8. Allow background/foreground execution (&) (5)
- * 9. Printing/reporting of background processes, (including the jobs command) (10)
- * 10. Allow file redirection (> and <) (5)
- * 11. Allow (1) pipe (|) (10)
- * 12. Supports reading commands from prompt and from file (10)
+ * [X] Run executables without arguments (10)
+ * [X] Run executables with arguments (10)
+ * [ ] set for HOME and PATH work properly (5)
+ * [?] exit and quit work properly (5)
+ * [ ] cd (with and without arguments) works properly (5)
+ * [ ] PATH works properly. Give error messages when the executable is not found (10)
+ * [ ] Child processes inherit the environment (5)
+ * [ ] Allow background/foreground execution (&) (5)
+ * [ ] Printing/reporting of background processes, (including the jobs command) (10)
+ * [ ] Allow file redirection (> and <) (5)
+ * [ ] Allow (1) pipe (|) (10)
+ * [ ] Supports reading commands from prompt and from file (10)
  */
 
 #define MAX_LENGTH 1024
@@ -28,10 +28,10 @@ int main(int argc, char **argv, char **envp) {
 		if(!fgets(line, MAX_LENGTH, stdin)) //fgets => reads all input in string into 'line'
 			break;
 
-		
+		if((line == "exit") || (line == "quit"))
+			exit(0);
 		system(line);
 	}
 	
 	return 0;
-
 }
